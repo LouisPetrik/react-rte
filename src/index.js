e7b1e39533e172baa4b37d027648fa3010bc4930
Louis <33809479+LouisPetrik@users.noreply.github.com>
2019-02-09 21:59:55 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PageHeader extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+          <header>
+              <h1>Hallo, mit Lizenz!</h1>
+          </header>
+      </React.Fragment>
+    );
+  }
+}
+export default PageHeader;
